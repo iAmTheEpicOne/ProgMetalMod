556ffc58223d5bf429f41ea9ab9872cf96f9795e
iAmTheEpicOne <avidprogfan@gmail.com>
2018-06-06 22:45:50 -0500

    interface fix check_provider print statement

--- interface.py
+++ interface.py
@@ -36,7 +36,7 @@ def check_provider(submission):
     # Checks provider of link and print provider name
     domains = ["YouTube", "BandCamp", "Spotify", "SoundCloud"]
     if submission.provider_name in domains:
-        print submission.provider_name
+        print(submission.provider_name)
 
 def get_submission_age(submission):
     # Returns a delta time object from the difference of the current time and the submission creation time

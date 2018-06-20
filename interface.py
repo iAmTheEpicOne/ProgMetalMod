82e0f1c9d3da1c31cef0bc122ac1973e96e63480
iAmTheEpicOne <avidprogfan@gmail.com>
2018-06-20 01:05:15 -0500

    main: temp check post only, interface: check post/link titles

--- interface.py
+++ interface.py
@@ -7,7 +7,10 @@ import logging.handlers
 import logging
 import os
 import re
+import musicbrainzngs
 
+log = logging.getLogger("bot")
+logging.getLogger("musicbrainzngs")
 
 def check_post(submission):
     # True if not archived and not self.post
@@ -32,8 +35,16 @@ def check_self(submission):
     # True if post is self.post
     return submission.is_self
 
-# .json "removed" value isn't available for non-removed posts
+def check_domain(domain):
+    # Checks if link's domain is in accepted domain list
+    domains = ["youtube.com", "youtu.be", "m.youtube.com", "open.spotify.com", "bandcamp.com", "soundcloud.com"]
+    if domain in domains:
+        return True
+    else:
+        return False
+
 def check_removed(submission):
+    # .json "removed" value isn't available for non-removed posts
     # True if post is banned_by mod
     if submission.banned_by is "null":
         return False
@@ -56,114 +67,143 @@ def get_url(submission):
     else:
         return submission.url
 
-
+def get_musicbrainz_result(artist, song):
+    # Checks artist and song info against musicbrainz database
+    # Currently only returns True or False value
+    result = musicbrainzngs.search_recordings(artist=artist, recording=release)
+    # If the artist and song matches a recording in database then return True
+    if not result['release-list']:
+        return False
+    return True
+    
 def get_domain(submission):
-    # Checks domain of link and print submission and domain name
-    # Returns domain name
-    # domains = ["youtube.com", "youtu.be", "open.spotify.com", "bandcamp.com", "soundcloud.com"]
-    name = re.search('(spotify.com|bandcamp.com|soundcloud.com|youtube.com|youtu.be)', submission.domain)
-    if not name is None:
-        return name.group(0)
-    else:
-        return submission.domain
+    return submission.domain
 
-def get_title(reddit, submission, reports):
+def get_link_title(reddit, submission):
+    # Get 'Artist' and 'Song' from the embedded link info
+    # Returns [artist, song] or 'None' if soundcloud link (for now)
+    # Note that artist=None if auto-generated YouTube channel "Various Artist - Topic"
+    # None that song=None and artist=link_title if YouTube video title didn't match regex
+    #   link_title can then be used as crosscheck with reddit post title info
     domain = get_domain(submission)
     if domain is "spotify.com":
         description = submission.media.oembed.description
-        song, band = description.split(", a song by ", 1)
-        extra = re.search(' on Spotify', band)
-        extra = extra.group(0)
-        band = band[:extra.start()]
-        if song not in submission.title or band not in submission.title:
-            # song or band is not in submission title
-            #   so title has bad format
-            if reports is 1:
-                rule_bad_title(reddit, submission)
-        title = band + " - " + song
+        title = re.search('(.*), a song by (.*) on Spotify', description)
+        song = title.group(1).encode('utf-8')
+        artist = title.group(2).encode('utf-8')
+        link_title = [artist, song]
     elif domain is "bandcamp.com":
         description = submission.media.oembed.title
-        song, band = description.split(", by ", 1)
-        if song not in submission.title or band not in submission.title:
-            # song or band is not in submission title
-            #   so title has bad format
-            if reports is 1:
-                rule_bad_title(reddit, submission)
-        title = band + " - " + song
-    else:
-        # REGEX OVERLOAD INCOMING
-        # Use regexr.com with regex in ' ' and check all the titles it catches
-        # re.search will store Artist in title.group(1) and Song in title.group(2)
-        title = re.search('(?:(?:^[()[\]{}|].*?[()[\]{}|][\s|\W]*)|(?:^))(.*?)\s?(?:-{1,2}|\u2014|\u2013)\s?(?:"|)([^"]*?)(?:\/\/.*|\\.*|["].*|\s?[-([\s]\s?(?:drum|guitar|bass|vocal|voice|playthrough|ffo|official|new|metal|prog).*|)(?:(?:(?:\s?[()[\]{}|]).*?[()[\]{}|]?.?$)|(?:$))', submission.title)
-        if title is None:
-            #ah fuck it didn't work
-            if reports is 1:
-                rule_bad_title(reddit, submission)
-        #title = re.search('^.+?\s(?:-{1,2}|\u2014|\u2013)\s.*$', submission.title)
-        #if title is None:
-        #    if reports is 1:
-        #        rule_bad_title(reddit, submission)
-        #    title = submission.title
-        #else:
-        #    title = title.group(0)
-        #extra = re.search('\s[()[\]{}|].*[()[\]{}|].*$', title)
-        #if not extra is None:
-        #    title = title[:extra.start()] + title[extra.end():]
-    return title.encode('utf-8')
-'''
-getting proper title of youtube and soundcloud links is going to be difficult
-    elif domain is "soundcloud.com:
-        
+        title = re.search('(.*), by (.*)', description)
+        song = title.group(1).encode('utf-8')
+        artist = title.group(2).encode('utf-8')
+        link_title = [artist, song]
     elif domain is "youtube.com" or domain is "youtu.be":
-        #description = submission.title
-        
-        author = submission.media.oembed.author_name
+    # Need to add YouTube API for better info
+    # Currently cannot access a video's description
+        link_author = submission.media.oembed.author_name
+        link_title = submission.media.oembed.title
+        if " - Topic" in link_author:
+        # YouTube channel is auto-generated "Artist - Topic"
+        # so video title is the song name
+            song = link_title
+            if "Various Artists" in link_author:
+            # YouTube channel is "Various Artist - Topic"
+            # so artist name is unknown
+            # need to use YouTube api to access video description to get correct artist name
+                artist = None
+            else:
+                topic = re.search('(.*) - Topic', author)
+                artist = topic.group(1)
+            link_title = [artist, song]
+        # If video is normal upload by label or user
+        else:
+            title = re.search('^(.*?)\s?(?:-{1,2}|\u2014|\u2013)\s?(?:"|)(\(?[^"]*?)\s?(?:["].*|(?:\(|\[|{).*[^)]$|[-([].*?(?:album|official|premiere|lyric|playthrough|single).*|$|\n)', link_title)
+            if title is None:
+                return [link_title, None]
+            else:
+                artist = title.group(1).encode('utf-8')
+                song = title.group(2).encode('utf-8')
+                link_title = [artist, song]
+    elif domain is "soundcloud.com":
+    # Need to add SoundCloud API for info
+        return None
+    return link_title
 
-        if " - Topic" in author:
-            if "Various Artists" in author:
-                # YouTube channel is "Various Artists - Topic"
-                #   so song name is media.oembed.title
-                song = submission.media.oembed.title
-                description = submission.title
-                if song not in description:
-                    # song is not in the submission title
-                    #   so title has bad format
-                    if reports is 1:
-                        rule_bad_title(reddit, submission)
-                extra = re.search('(\(|\[).*?(\)|\])', description)
-                if not extra is None:
-                    description = description[:extra.start()] + description[extra.end():]
-            topic = re.search(" - Topic", author)
-            topic = topic.group(0)
-            author = author[:topic.start()]
-        description = submission.media.oembed.title
-        
-        elif author in submission.title:
-
-
-'''
-
-
-def rule_violation(rule):
-    if not 'rules_violated' in locals():
-        rules_violated = []
-    
-
-def rule_six_month(reddit, submission, sub):
-    print("Rule Violation (6-month Repost): Reporting {}".format(submission.shortlink))
-    # submission.mod.remove()
-    # submission is post in violation
-    # sub is original unreposted post
-    submission.report("ProgMetalBot - Repost! Repost!")
-    reddit.redditor(settings.USER_TO_MESSAGE).message("ProgMetalBot", "I DID A THING\n\nPlease look at [this post]({}) for a possible repost or check the modmail.".format(submission.shortlink))
-    reddit.subreddit(settings.REDDIT_SUBREDDIT).message("ProgMetalBot - Song Repost Report", "Please look at [this post]({}) for a possible repost of [this post]({}); if I haven't screwed up then the post is in violation of the 6-month rule.\n\nThank you!\n\n With humble gratitude, ProgMetalBot v0.1".format(submission.shortlink, sub.shortlink))
+def get_post_title(submission):
+    # REGEX OVERLOAD INCOMING
+    # Use regex string in ' ' on regexr.com and check out all the titles it catches!
+    # re.search will store 'Artist' in title.group(1) and 'Song' in title.group(2)
+    title = re.search('(?:(?:^[()[\]{}|].*?[()[\]{}|][\s|\W]*)|(?:^))(.*?)\s?(?:-{1,2}|\u2014|\u2013)\s?(?:"|)(\(?[^"]*?)\s?(?:\/\/.*|\\\\.*|\|\|.*|\|.*\||["].*|(?:\(|\[|{).*[^)]$|[-([|:;].*?(?:favorite|video|full|tour|premier|released|cover|album|drum|guitar|bass|vocal|voice|playthrough|ffo|official|new|metal|prog).*|$|\n)', submission.title)
+    if title is None:
+        #ah fuck it didn't work
+        return None
+    else:
+        artist = title.group(1).encode('utf-8')
+        song = title.group(2).encode('utf-8')
+        post_title = [artist, song]
+        return post_title
+    #title = re.search('^.+?\s(?:-{1,2}|\u2014|\u2013)\s.*$', submission.title)
+    #if title is None:
+    #    if reports is 1:
+    #        rule_bad_title(reddit, submission)
+    #    title = submission.title
+    #else:
+    #    title = title.group(0)
+    #extra = re.search('\s[()[\]{}|].*[()[\]{}|].*$', title)
+    #if not extra is None:
+    #    title = title[:extra.start()] + title[extra.end():]
 
 def rule_bad_title(reddit, submission):
-    print("Rule Violation (Bad Title): Reporting {}".format(submission.shortlink))
     # Submission was found to have an incorrect title
-    submission.report("ProgMetalBot - Bad Title Format")
+    # Submission will be reported and message sent to mods
+    print("Rule Violation (Bad Title): Reporting {}".format(submission.shortlink))
+    # ***UNCOMMENT LATER***
+    #submission.report("ProgMetalBot - Bad Title Format")
     reddit.redditor(settings.USER_TO_MESSAGE).message("ProgMetalBot", "Please look at [this post]({}) to check for proper title format or check the modmail.".format(submission.shortlink))
-    reddit.subreddit(settings.REDDIT_SUBREDDIT).message("ProgMetalBot - Bad Title Format", "Please look at [this post]({}) and check for a proper title format.\n\nThank you!\n\nWith humble gratitude, ProgMetalBot v0.1".format(submission.shortlink))
+    # ***UNCOMMENT LATER***
+    #reddit.subreddit(settings.REDDIT_SUBREDDIT).message("ProgMetalBot - Bad Title Format", "Please look at [this post]({}) and check for a proper title format.\n\nThank you!\n\nWith humble gratitude, ProgMetalBot".format(submission.shortlink))
+
+def rule_bad_title_report(reddit, submission):
+    # Submission was found to possibly have an incorrect title
+    # Submission will only be reported to mods for verification
+    print("Possible Rule Violation (Bad Title): Reporting {}".format(submission.shortlink))
+    # ***UNCOMMENT LATER***
+    #submission.report("ProgMetalBot - Possible Bad Title/Link Match")
+    reddit.redditor(settings.USER_TO_MESSAGE).message("ProgMetalBot", "Please look at [this post]({}) to check for proper match of submission title and linked song or check the modmail.".format(submission.shortlink))
+    # ***UNCOMMENT LATER***
+    #reddit.subreddit(settings.REDDIT_SUBREDDIT).message("ProgMetalBot - Bad Title Format", "Please look at [this post]({}) and check for a proper match of submission title and linked song.\n\nThank you!\n\nWith humble gratitude, ProgMetalBot".format(submission.shortlink))
+
+def rule_six_month(reddit, submission, sub):
+    # Submission was found to violate the 'repost in six months' rule
+    # Submission will be reported and message sent to mods
+    print("Rule Violation (6-month Repost): Reporting {}".format(submission.shortlink))
+    #submission.mod.remove()
+    # ***UNCOMMENT LATER***
+    #submission.report("ProgMetalBot - Repost! Repost!")
+    reddit.redditor(settings.USER_TO_MESSAGE).message("ProgMetalBot", "I DID A THING\n\nPlease look at [this post]({}) for a possible repost or check the modmail.".format(submission.shortlink))
+    # ***UNCOMMENT LATER***
+    #reddit.subreddit(settings.REDDIT_SUBREDDIT).message("ProgMetalBot - Song Repost Report", "Please look at [this post]({}) for a possible repost of [this post]({}); if I haven't screwed up then the post is in violation of the 6-month rule.\n\nThank you!\n\n With humble gratitude, ProgMetalBot".format(submission.shortlink, sub.shortlink))
+
+def rule_violation(rules_violated, rule):
+    # Appends a rule to rules_violated for specific submission
+    rules_violated += [rule]
+    return rules_violated
+    
+def perform_mod_actions(reddit, rules_violated):
+    # For each rule in rules_violated, perform mod actions
+    if len(rules_violated) > 1:
+        submission = rules_violated[0]
+        for rule in rules_violated[1:]:
+            if rule is 1:
+            # bad title
+                rule_bad_title(reddit, submission)
+            elif 2 in rule:
+            # possible bad title - report only
+                rule_bad_title_report(reddit, submission)
+            elif 3 in rule:
+            # six month repost
+                rule_six_month(reddit, submission, rule[1])
 
 def initialize_link_array(reddit):
     # Initializes the link array of all past submissions
@@ -188,12 +228,58 @@ def update_stored_posts(stored_posts):
         for submission in stored_posts:
             f.write(submission + "\n")
 
+def check_submission(reddit, submission):
+    # Check the submission and link information for bad title
+    # Does not check against the stored posts
+    # Artist and Song name verification happens here with checks against submission title
+    link_domain = get_domain(submission)
+    if not check_domain(link_domain):
+        # could check domain against secondary list including facebook, twitter, metal magazines, etc.
+        continue
+    rules_violated = [submission]
+    post_title = submission.title
+    post_info = get_post_title(submission)
+    if post_info is None:
+        # add rule violation for bad title
+        rules_violated = rule_violation(rules_violated, 1)
+        perform_mod_actions(reddit, rules_violated)
+        rules_violated = []
+        continue
+    else:
+        post_artist = post_info[0]
+        post_song = post_info[1]
+    link_info = get_link_title(submission)
+    if link_info is None:
+        # None means soundcloud link which is not handled yet
+        continue
+    else:
+        link_artist = link_info[0]
+        link_song = link_info[1]
+    if link_artist is None:
+        # auto-generated YouTube channel "Various Artist - Topic"
+        # artist unknown until YouTube API enabled, song is known
+        if link_song is not post_song:
+            # Report for link info not matching post info
+            rules_violated = rule_violation(rules_violated, 2)
+    elif link_song is None:
+        # YouTube video title didn't match regex, so link_artist is full video title
+        # Can check post_info against this info
+        video_title = link_artist
+        if post_artist not in video_title or post_song not in video_title:
+            # Report for artist or song in post title not found in link title
+            rules_violated = rule_violation(rules_violated, 2)
+    elif post_artist is not link_artist or post_song is not link_song:
+            # Report for artist or song in post title not found in link title
+            rules_violated = rule_violation(rules_violated, 2)
+    perform_mod_actions(reddit, rules_violated)
+    rules_violated = []
+
 def check_list(reddit, submission, stored_posts):
     # Check if a submission url is in the list:
     # If it is, report post to mods of subreddit
     # If not, add it to the list
     # Store submission in stored_posts if not already stored
-    # **Unsure if redundent submissions are added to stored_posts**
+    # **Unsure if redundant submissions are added to stored_posts**
     #if submission.url not in [sub.url for sub in stored_posts] or submission in stored_posts:
     
     # Check if exact url already exists
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import praw
+import musicbrainzngs
 import time
 import interface
 import settings
@@ -14,15 +15,14 @@ SLEEP = 600
 
 
 # LOGGING CONFIGURATION
-LOG_LEVEL = logging.INFO
 LOG_FILENAME = "bot.log"
 LOG_FILE_BACKUPCOUNT = 5
 LOG_FILE_MAXSIZE = 1024 * 256
 
 
 # LOGGING SETUP
-log = logging.getLogger("bot")
-log.setLevel(LOG_LEVEL)
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger("bot").setLevel(logging.INFO)
 log_formatter = logging.Formatter('%(levelname)s: %(message)s')
 log_formatter_file = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 log_stderrHandler = logging.StreamHandler()
@@ -33,36 +33,49 @@ if LOG_FILENAME is not None:
 	                                                       backupCount=LOG_FILE_BACKUPCOUNT)
 	log_fileHandler.setFormatter(log_formatter_file)
 	log.addHandler(log_fileHandler)
-	
+# Musicbrainz logging
+logging.getLogger("musicbrainzngs").setLevel(logging.INFO)
 
 # MAIN PROCEDURE
 def run_bot():
-    reddit = praw.Reddit(user_agent='ProgMetalBot 0.1',
+    
+    #api and authentication variables
+    app_useragent_version = os.environ['APP_USERAGENT'] + ' ' + os.environ['APP_VERSION']
+    reddit = praw.Reddit(user_agent=app_useragent_version,
                          client_id=os.environ['REDDIT_CLIENT_ID'],
                          client_secret=os.environ['REDDIT_CLIENT_SECRET'],
                          password=os.environ['REDDIT_PASSWORD'],
                          username=os.environ['REDDIT_USERNAME'])
-                         
+    musicbrainzngs.auth(os.environ['MUSICBRAINZ_USERNAME'],
+                        os.environ['MUSICBRAINZ_PASSWORD'])
+    musicbrainzngs.set_useragent(os.environ['APP_USERAGENT'],
+                                 os.environ['APP_VERSION'],
+                                 os.environ['CONTACT_EMAIL'])
+    
     subreddit = reddit.subreddit(settings.REDDIT_SUBREDDIT)
     
-    log.info("Gathering posts from subreddit %s", settings.REDDIT_SUBREDDIT)
-    stored_posts = interface.initialize_link_array(reddit)
-    
+    #log.info("Gathering posts from subreddit %s", settings.REDDIT_SUBREDDIT)
+    #stored_posts = interface.initialize_link_array(reddit)
     log.info("Start bot for subreddit %s", settings.REDDIT_SUBREDDIT)
     while True:
         try:
             log.info("Reading stream of submissions for subreddit %s", settings.REDDIT_SUBREDDIT)
             for submission in subreddit.stream.submissions():
-                # For each submission, check if it is younger than MAX_REMEMBER_LIMIT
-                #print(submission.title) # to make it non-lazy
-                #pprint.pprint(vars(submission))
-                #break
-                if interface.check_post(submission) and submission not in stored_posts:
-                    # Remove links > MAX_REMEMBER_LIMIT
+                
+                # Checks submission against stored posts from last 6 months
+                # Checks submission for accurate title/link info
+                #if interface.check_post(submission) and submission not in stored_posts:
+                #    # Remove links > MAX_REMEMBER_LIMIT
+                #    log.info("Found new post in subreddit %s", settings.REDDIT_SUBREDDIT)
+                #    stored_posts = interface.purge_old_links(stored_posts)
+                #    interface.check_submission(reddit, submission)
+                #    stored_posts = interface.check_list(reddit, submission, stored_posts)
+                
+                # Only checks submission for accurate title/link info
+                if interface.check_post(submission):
                     log.info("Found new post in subreddit %s", settings.REDDIT_SUBREDDIT)
-                    stored_posts = interface.purge_old_links(stored_posts)
-                    stored_posts = interface.check_list(reddit, submission, stored_posts)
-            
+                    interface.check_submission(reddit, submission)
+                    
             # Write stored posts to a file
             #interface.update_stored_posts(stored_posts)
 

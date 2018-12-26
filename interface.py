4f47294493a2d4b06269ce78783ae04a99305a25
iAmTheEpicOne <avidprogfan@gmail.com>
2018-12-25 20:39:21 -0600

    praw API Exception, not HTTP Exception

--- interface.py
+++ interface.py
@@ -512,7 +512,7 @@ def check_list(reddit, submission):
                         log.info("Title match of \"{}\" and \"{}\"".format(query, result_title))
                         rule_six_month(reddit, submission, search_result)
                         break
-    except praw.errors.HTTPException as e:
+    except praw.exceptions.APIException as e:
         # HTTP Exception, will skip current search
         #e._raw.status_code will show 503, etc.
         log.error("Exception in reddit search: %s", e, exc_info=True)
